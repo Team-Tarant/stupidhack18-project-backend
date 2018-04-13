9b0161bb413968412b66bc441bb2bac568e64553
Hugo Holmqvist <hugo.holm@live.com>
2018-04-13 20:51:48 +0300

    Split directions into steps

--- src/models/Route.ts
+++ src/models/Route.ts
@@ -1,3 +1,33 @@
+
+
+class Step {
+  type: {
+    'start',
+    'home',
+    'alko',
+    'fast_food',
+    'bar',
+    'grocery'
+  } = null;
+  start_location: {
+    lat: number,
+    lng: number
+  } = null;
+  end_location: {
+    lat: number,
+    lng: number
+  } = null;
+  directions: Direction[] = null;
+
+  constructor(obj) {
+    Object.keys(this).forEach(key => {
+      if (key === 'directions') {
+        this.directions = obj['steps'];
+      } else this[key] = obj[key];
+    });
+  }
+}
+
 class Direction {
   distance: {
     text: string,
@@ -28,15 +58,13 @@ class Direction {
 export default class Route {
   waypoint_order: number[] = null;
   summary: string = null;
-  directions: Direction[] = null
+  steps: Step[] = null
 
   constructor(obj) {
     Object.keys(this).forEach(key => {
-      if (key === 'directions') {
-        this.directions = obj['legs']
-          .map(o => o.steps)
-          .reduce((a, b) => a.concat(b))
-          .map(o => new Direction(o));
+      if (key === 'steps') {
+        this.steps = obj['legs']
+          .map(o => new Step(o));
       } else this[key] = obj[key];
     });
   }
--- src/services/RouteService.ts
+++ src/services/RouteService.ts
@@ -5,7 +5,7 @@ export default class RouteService {
   constructor() {}
 
   async findALITRouteToDestination(): Promise<Route> {
-    let res = await axios.default.get('https://maps.googleapis.com/maps/api/directions/json?origin=Leppasuonkatu%2011,%20Helsinki&destination=Opastinsilta%202%20a&mode=walking&waypoints=McDonalds%20Vantaa,%20Helsinki|Alko%20Pasila,%20Helsinki&key=AIzaSyDSbvTAIB6YD0hE26dIbzvBhM7-tRpapGI');
+    let res = await axios.default.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Leppasuonkatu%2011,%20Helsinki&destination=Opastinsilta%202%20a&mode=walking&waypoints=McDonalds%20Vantaa,%20Helsinki|Alko%20Pasila,%20Helsinki&key=${process.env.GOOGLE_MAPS_API_KEY}`);
     let route = new Route(res.data.routes[0]);
     return route;
 
