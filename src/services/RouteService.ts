7834568f425dca10036fe985b4eb38c404fd105f
Sasu Mäkinen <sasu.makinen@helsinki.fi>
2018-04-13 21:48:22 +0300

    places systeemii

--- src/services/RouteService.ts
+++ src/services/RouteService.ts
@@ -1,13 +1,35 @@
-import * as axios from 'axios';
-import Route from '../models/Route';
+import * as axios from 'axios'
+import Route from '../models/Route'
 
 export default class RouteService {
   constructor() {}
 
-  async findALITRouteToDestination(): Promise<Route> {
-    let res = await axios.default.get(`https://maps.googleapis.com/maps/api/directions/json?origin=Leppasuonkatu%2011,%20Helsinki&destination=Opastinsilta%202%20a&mode=walking&waypoints=McDonalds%20Vantaa,%20Helsinki|Alko%20Pasila,%20Helsinki&key=${process.env.GOOGLE_MAPS_API_KEY}`);
-    let route = new Route(res.data.routes[0]);
-    return route;
-
+  async findALITRouteToDestination(location): Promise<Route> {
+    const params = {
+      location: location,
+      rankby: 'distance',
+      type: 'restaurant|liquor_store|bar|park|food'
+    }
+    let places = await axios.default.get(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${
+        process.env.GOOGLE_MAPS_API_KEY
+      }&location=${params.location}&rankby=${params.rankby}&type=${params.type}`
+    )
+    let placesArray = places.data.results
+    let randomNumbers = []
+    let i = 0
+    while (i <= Math.floor(Math.random() * 10) + 3) {
+      i++
+      randomNumbers.push(Math.floor(Math.random() * placesArray.length))
+    }
+    let locationIDs = randomNumbers.map(idx => placesArray[idx].place_id)
+    let waypoints = locationIDs.join('|')
+    let res = await axios.default.get(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=Leppasuonkatu%2011,%20Helsinki&destination=Opastinsilta%202%20a&mode=walking&waypoints=${waypoints}&key=${
+        process.env.GOOGLE_MAPS_API_KEY
+      }`
+    )
+    let route = new Route(res.data.routes[0])
+    return route
   }
 }
